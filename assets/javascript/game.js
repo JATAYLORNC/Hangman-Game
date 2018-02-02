b868f83a09fbc9dc959f60e02af13400155d2c57
JATAYLORNC <disneyjeff@gmail.com>
2018-02-02 18:07:18 -0500

    Code functions properly but without game reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,128 +1,159 @@
-var wordArray = ["Pooh", "Piglet", "Eyore", "Lilo", "Stitch", "Cinderella", "Jasmine"];
-
-var gameOver = 0;
-
-var gamesPlayed = 0;
-
-var wins = 0, losses = 0;
 
 
-//Use while loop so that game can be played multiple times until all words have been used.
-while (wordArray.length > 0) {
+window.onload = function hangMan() {
 
-    //initialize array of letters already guessed by user
-    var previousGuess = [];
+//window.addEventListener("onload", hangMan()); <-----html will not even load with this
 
-    //initialize counter for numer of guesses
-    var guessCount = 10;
+//document.addEventListener("onload", hangMan()); //html loads but null value error occurs at line 43.
 
-    //declare newGuessBoolean
-    var newGuessBoolean;
+//window.onload = hangMan();//html loads but null value error occurs at line 43.
 
-    //Select game word
-    var gameWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+// function hangMan() {
 
-    //Display game word on screen with letters hidden
-    for (i=0; i<gameWord.length; i++) {
-        
-        var displayID = "ltr" + i.toString();
-    
-        document.getElementById(displayID).innerHTML = gameWord.charAt(i);
-    }
+    var wordArray = ["Pooh", "Piglet", "Eyore", "Lilo", "Stitch", "Cinderella", "Jasmine"];
 
-    //Remove word from array so that it cannot be chosen again for additional games played
-    
-    
-    wordArray.splice(wordArray.indexOf(gameWord), 1);
+    var gameOver = 0;
 
-    gameWord = gameWord.toLowerCase();
+    var gamesPlayed = 0;
 
-    //increment game counter by 1
-    gamesPlayed++;
+    var wins = 0, losses = 0;
 
-    //display new count of games played
+    //Use while loop so that game can be played multiple times until all words have been used.
+    if (wordArray.length > 0) {
 
-    //loop until game is over
-    while (gameOver==0) {
+        //initialize array of letters already guessed by user
+        var previousGuess = [];
 
-        newGuessBoolean = 1;
-           
-        //loop until the key chosen is a letter and has not already been guessed
-        while (newGuessBoolean == 1) {
+        //initialize counter for numer of guesses
+        var guessCount = 10;
 
+        //Select game word
+        var gameWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+
+        console.log (gameWord);
+
+        gameWord = gameWord.toLowerCase();
+
+        var gameWordArray = gameWord.split("");
+
+        //Remove word from array so that it cannot be chosen again for additional games played   
+        wordArray.splice(wordArray.indexOf(gameWord), 1);
+
+        //Display game word on screen with letters hidden
+        for (i=0; i<gameWord.length; i++) {
+
+            var wordBlock = document.getElementById("displayWord");
+            
+            var hdn = document.createElement("span");
+
+            wordBlock.appendChild(hdn);
+
+            var att = document.createAttribute("id");
+
+            att.value = "ltr" + i.toString();
+
+            hdn.setAttributeNode(att);
+
+            hdn.innerHTML = "________   "
+            
+        }
+
+        //increment game counter by 1
+        gamesPlayed++;
+
+        //display new count of games played
+        document.getElementById("gmsPlayed").innerHTML = gamesPlayed;
+
+        //condition true until game is over
+        if (gameOver == 0) {
+
+            var correctGuess = 0;
+                
             // This function is run whenever the user presses a key.
             document.onkeyup = function(event) {
 
                 // Determines which key was pressed.
-                var userGuess = String.fromCharCode(event.which).toLowerCase();
+                //var userGuess = String.fromCharCode(event.which).toLowerCase();
+                var userGuess = event.key;
 
                 //assigns the key code to a variable
-                var userKeyCode = event.which;
+                var userKeyCode = Number(event.keyCode);
 
                 //Make sure key pressed is a letter of the alphabet
-                if (userKeyCode >= 65 && userKeyCode <= 90) {
+                if (userKeyCode >= 65 && userKeyCode <= 97) {
 
                     //Make sure key pressed is not one of the previous guesses
                     if (previousGuess.indexOf(userGuess) >= 0) {
                         alert("This letter has already been guessed.  Please choose another letter");
                     } else {
-                        //change newGuessBoolean
-                        newGuessBoolean = 0;
-
-                        //decrement guessCount
-                        guessCount--;
 
                         //add letter to previousGuess array    
-                        previousGuess.push(userGuess);    
-                    } 
+                        previousGuess.push(userGuess); 
+                        
+                        if (gameWord.indexOf(userGuess) == -1) {
+                            alert("Letter guessed is not in the word.");
+
+                            //decrement guessCount
+                            guessCount--;
+
+                            //display guessCount
+                            document.getElementById("gsCount").innerHTML = guessCount;
+
+
+                        } else {
+                            
+                            //account for multiples of the same letter in the game word
+                            for (j=0; j<gameWord.length; j++) {
+            
+                                if (userGuess == gameWordArray[j]) {
+
+                                    var ltrBlock = document.getElementById("ltr" + j.toString());
+
+                                    correctGuess++;
+            
+                                    if ( j == 0) {            
+                                        ltrBlock.innerHTML = gameWordArray[j].toUpperCase();
+                                    } else {
+                                        ltrBlock.innerHTML = gameWordArray[j];   
+                                    }
+                                }
+                            }                        
+                        }
+                    }   
+                              
                 } else {
                     alert("Key pressed is not a letter.  Please try again.");
                 }
-            }
-        }
 
-        if (gameWord.indexOf(userGuess) == -1) {
-            alert("Letter guessed is not in the word.");
-        } else {
-            //display letter on screen
+                //check to see if all letters in word have been guessed
+                if (correctGuess == gameWord.length) {
+                    alert ("Congratulations on guessing the word correctly!  Why not play again?");
+                    
+                    //increment wins counter
+                    wins = wins + 1;
 
-            //remove letter from gameWord string
+                    //display new wins count
+                    document.getElementById("gmsWon").innerHTML = wins;
 
-            //account for multiples of the same letter
+                    //exit game loop
+                    gameOver = 1;
+                }
 
-        }
+                //check to see if the guess count is 0
+                if (guessCount == 0) {
+                    alert ("Game Over!  Why not try again?");
 
-        //check to see if all letters in word have been guessed
-        if (gameword.length == 0) {
-            alert ("Congratulations on guessing the word correctly!  Why not play again?");
-            
-            //increment wins counter
-            wins = wins + 1;
+                    //increment losses counter
+                    losses = losses + 1          
 
-            //increment number of games played
-            gamesPlayed = gamesPlayed + 1;
+                    //display new losses count
+                    document.getElementById("gmsLost").innerHTML = losses;
 
-            //display new wins count
-
-            //exit game loop
-            gameOver = 1;
-        }
-
-        //check to see if the guess count is 0
-        if (guessCount == 0) {
-            alert ("Game Over!  Why not try again?");
-
-            //increment losses counter
-            losses = losses + 1
-
-            //increment number of games played
-            gamesPlayed = gamesPlayed + 1;            
-
-            //display new losses count
-
-            //exit game loop
-            gameOver = 1;
-        }
+                    //exit game loop
+                    gameOver = 1;
+                    
+                }     
+            }      
+        }       
     }
-
 }
