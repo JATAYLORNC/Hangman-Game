c048eaa54b2cf01e205fd662e55e4088f322fbbf
JATAYLORNC <disneyjeff@gmail.com>
2018-02-05 11:05:43 -0500

    Game code fully functional without errors.  Page needs minor style change.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,72 @@
 
 
-window.onload = function hangMan() {
-
-//window.addEventListener("onload", hangMan()); <-----html will not even load with this
-
-//document.addEventListener("onload", hangMan()); //html loads but null value error occurs at line 43.
-
-//window.onload = hangMan();//html loads but null value error occurs at line 43.
-
-// function hangMan() {
+window.onload = function startGame() {
 
     var wordArray = ["Pooh", "Piglet", "Eyore", "Lilo", "Stitch", "Cinderella", "Jasmine"];
-
-    var gameOver = 0;
 
     var gamesPlayed = 0;
 
     var wins = 0, losses = 0;
 
-    //Use while loop so that game can be played multiple times until all words have been used.
-    if (wordArray.length > 0) {
+    var gameOver = false;
 
-        //initialize array of letters already guessed by user
-        var previousGuess = [];
+    hangMan();
 
-        //initialize counter for numer of guesses
-        var guessCount = 10;
+    function hangMan() {
 
-        //Select game word
-        var gameWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    //Use if statement so that game can be played multiple times until all words have been used.
+        if (wordArray.length > 0) {
 
-        console.log (gameWord);
+            console.log(wordArray.length);
 
-        gameWord = gameWord.toLowerCase();
+            //initialize array of letters already guessed by user
+            var previousGuess = [];
 
-        var gameWordArray = gameWord.split("");
+            //initialize counter for numer of guesses
+            var guessCount = 10;
 
-        //Remove word from array so that it cannot be chosen again for additional games played   
-        wordArray.splice(wordArray.indexOf(gameWord), 1);
+            //display guess count
+            document.getElementById("gsCount").innerHTML = guessCount
 
-        //Display game word on screen with letters hidden
-        for (i=0; i<gameWord.length; i++) {
+            //Select game word
+            var gameWordIndex = Math.floor(Math.random() * wordArray.length);
+            var gameWord = wordArray[gameWordIndex];
 
-            var wordBlock = document.getElementById("displayWord");
-            
-            var hdn = document.createElement("span");
+            console.log (gameWord);
+            console.log(gameWordIndex);
 
-            wordBlock.appendChild(hdn);
+            gameWord = gameWord.toLowerCase();
 
-            var att = document.createAttribute("id");
+            var gameWordArray = gameWord.split("");
 
-            att.value = "ltr" + i.toString();
+            //Remove word from array so that it cannot be chosen again for additional games played   
+            wordArray.splice(gameWordIndex, 1);
 
-            hdn.setAttributeNode(att);
+            console.log(wordArray);
 
-            hdn.innerHTML = "________   "
-            
-        }
+            //Display game word on screen with letters hidden
+            for (i=0; i<gameWord.length; i++) {
 
-        //increment game counter by 1
-        gamesPlayed++;
+                var wordBlock = document.getElementById("displayWord");
+                
+                var hdn = document.createElement("span");
 
-        //display new count of games played
-        document.getElementById("gmsPlayed").innerHTML = gamesPlayed;
+                wordBlock.appendChild(hdn);
 
-        //condition true until game is over
-        if (gameOver == 0) {
+                var att = document.createAttribute("id");
 
+                att.value = "ltr" + i.toString();
+
+                hdn.setAttributeNode(att);
+
+                hdn.innerHTML = "__"
+                
+            }
+
+            //initialize letters guessed string
+            var ltrsGuessed = "";
+    
+            //initialize correctGuess count
             var correctGuess = 0;
                 
             // This function is run whenever the user presses a key.
@@ -79,6 +79,8 @@ window.onload = function hangMan() {
                 //assigns the key code to a variable
                 var userKeyCode = Number(event.keyCode);
 
+
+
                 //Make sure key pressed is a letter of the alphabet
                 if (userKeyCode >= 65 && userKeyCode <= 97) {
 
@@ -89,6 +91,12 @@ window.onload = function hangMan() {
 
                         //add letter to previousGuess array    
                         previousGuess.push(userGuess); 
+
+                        ltrsGuessed = ltrsGuessed + " " + userGuess;
+
+                        //display letter in letters guessed
+                        document.getElementById("ltrG").innerHTML=ltrsGuessed;
+
                         
                         if (gameWord.indexOf(userGuess) == -1) {
                             alert("Letter guessed is not in the word.");
@@ -108,26 +116,24 @@ window.onload = function hangMan() {
                                 if (userGuess == gameWordArray[j]) {
 
                                     var ltrBlock = document.getElementById("ltr" + j.toString());
-
-                                    correctGuess++;
             
                                     if ( j == 0) {            
                                         ltrBlock.innerHTML = gameWordArray[j].toUpperCase();
                                     } else {
-                                        ltrBlock.innerHTML = gameWordArray[j];   
+                                        ltrBlock.innerHTML = gameWordArray[j];
                                     }
+                                    correctGuess++;
                                 }
                             }                        
                         }
                     }   
-                              
+                            
                 } else {
                     alert("Key pressed is not a letter.  Please try again.");
                 }
 
                 //check to see if all letters in word have been guessed
                 if (correctGuess == gameWord.length) {
-                    alert ("Congratulations on guessing the word correctly!  Why not play again?");
                     
                     //increment wins counter
                     wins = wins + 1;
@@ -135,25 +141,89 @@ window.onload = function hangMan() {
                     //display new wins count
                     document.getElementById("gmsWon").innerHTML = wins;
 
-                    //exit game loop
-                    gameOver = 1;
+                    //increment game counter by 1
+                    gamesPlayed++;
+
+                    //display new count of games played
+                    document.getElementById("gmsPlayed").innerHTML = gamesPlayed;
+
+                    gameOver = true;
+
+                    //Restart game
+
+                    if(gameOver==true) {
+
+                        setTimeout(function() { alert("Congratulations on guessing the word correctly!  Why not play again"); }, 1000);
+
+                        setTimeout(function() {
+                            var parent = document.getElementById("displayWord");
+                            for (i = 0; i < gameWord.length; i++) {
+                                var child = document.getElementById("ltr" + i.toString());
+                                parent.removeChild(child);
+                            }
+                            document.getElementById("ltrG").innerHTML = "";
+                        }, 3000);
+
+                        restartGame = true;
+                    }
+
+                    if(restartGame == true) {
+                        setTimeout(hangMan, 3000);
+                    }    
                 }
 
                 //check to see if the guess count is 0
                 if (guessCount == 0) {
-                    alert ("Game Over!  Why not try again?");
-
+                    
                     //increment losses counter
                     losses = losses + 1          
 
                     //display new losses count
                     document.getElementById("gmsLost").innerHTML = losses;
 
-                    //exit game loop
-                    gameOver = 1;
-                    
+                    //increment game counter by 1
+                    gamesPlayed++;
+
+                    //display new count of games played
+                    document.getElementById("gmsPlayed").innerHTML = gamesPlayed;
+
+                    gameOver = true;
+
+                    //Restart game
+
+                    if(gameOver==true) {
+
+                        for (k=0; k<gameWord.length; k++) {
+            
+                            var ltrBlock = document.getElementById("ltr" + k.toString());
+    
+                            if ( k == 0) {            
+                                ltrBlock.innerHTML = gameWordArray[k].toUpperCase();
+                            } else {
+                                ltrBlock.innerHTML = gameWordArray[k];
+                            }
+                        }
+
+                        setTimeout(function() { alert("Game Over!  Why not try again?"); }, 1000);
+                        
+                        setTimeout(function() {
+                            var parent = document.getElementById("displayWord");
+                            for (i = 0; i < gameWord.length; i++) {
+                                var child = document.getElementById("ltr" + i.toString());
+                                parent.removeChild(child);
+                            }
+                            document.getElementById("ltrG").innerHTML = "";
+                        }, 3000);
+
+                        restartGame = true;
+                        console.log(restartGame);
+                    }
+
+                    if(restartGame == true) {
+                        setTimeout(hangMan, 3000);
+                    }                        
                 }     
-            }      
-        }       
+            }             
+        }
     }
 }
